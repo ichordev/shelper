aa878d9e76d93f21dc5e39c248e269ac9dc901b1
zxin <15670465+ichordev@users.noreply.github.com>
2024-10-24 04:14:49 +1000

    Multiple Shelper instances can now be used at once

--- README.md
+++ README.md
@@ -1,80 +1,90 @@
 # Shelper
 
-A helper library for loading, reloading, and unloading bgfx shaders.
+*Shader helper*&mdash;a helper library for loading, re-loading, and un-loading bgfx shaders.
 
 Depends on [BindBC-bgfx](https://github.com/BindBC/bindbc-bgfx) v1.0.0 or higher.
 
-## API
-The library's `shaderPath` variable indicates the location of your shader binaries. By default, it is set to `shaders/`.
+## Setup
+First, you'll need an instance of the `Shelper` struct template.
+```d
+import shelper;
 
-Shelper expects shaders to have the following file extensions:
+Shelper!() myShaders;
+```
+> [!NOTE]\
+> Mutable `Shelper` instances should only be passed around by reference.
+
+You can set the `path` property to tell Shelper the location of your shader binaries.
+It defaults to the relative path `shaders/`.
+```
+myShaders.path = "assets/shaders"; //same as "./assets/shaders/"
+```
+
+Shelper requires your shader binaries to have the `.bin` file extension.
+
+By default, Shelper also uses glslang-style naming-conventions to differentiate different shader types:
 - `*.vert.bin` for vertex shaders.
 - `*.frag.bin` for fragment shaders.
 - `*.comp.bin` for compute shaders.
 
-### Functions
+These can be re-configured when instantiating the `Shelper` struct template:
 ```d
-bgfx.ProgramHandle load(string vert, string frag);
+Shelper!("%s.vert",  "%s.frag",  "%s.comp") myShaders3; //the default
+Shelper!("%s.vsh",   "%s.fsh",   "%s.csh")  myShaders1; //a popular alternative
+Shelper!("vs_%s",    "fs_%s",    "cs_%s")   myShaders2; //the format of bgfx's examples
 ```
-Loads two shader files and creates a shader program. `vert` and `frag` should be only be a file name, without a file extension.
-Example:
+
+## Loading
+Once everything is set up to your liking, use `load` to open your shaders and create a shader program:
 ```d
-shaderPath = "shaders/";
-myProgram = shelper.load(
+auto myShaderProgram = myShaders.load(
 	"myShader", //will open "shaders/myShader.vert.bin"
 	"myShader", //will open "shaders/myShader.frag.bin"
 );
 ```
 
----
-
+Supply only one name to create a compute shader:
 ```d
-bgfx.ProgramHandle load(string comp);
+auto myComputeShader = myShaders.load(
+	"myShader", //will open "shaders/myShader.comp.bin"
+);
 ```
-Loads a shader file and creates a compute shader program. `comp` should be only be a file name, without a file extension.
 
----
-
+## Re-Loading
+When you need to reload a single shader program, pass its program handle into the `reload` function:
 ```d
-void reload(ref bgfx.ProgramHandle program);
+myShaders.reload(myShaderProgram);
 ```
-Reloads `program`'s shader files afresh from the disk and sets `program` to a newly created `bgfx.ProgramHandle`.
+This reloads `myShaderProgram`'s shaders afresh from the disk and sets `myShaderProgram` to a newly-created `bgfx.ProgramHandle`.
+
 > [!NOTE]\
-> This function always creates new underlying `bgfx.ShaderHandle`s, and the old ones will only be destroyed if no other Shelper-managed `bgfx.ProgramHandle` is using them. Because of this, it's best to use the version of the function below when reloading more than one shader.
+> This function always creates new underlying `bgfx.ShaderHandle`s, but the old ones will only be destroyed if no other `bgfx.ProgramHandle` in the same Shelper instance is using them.
+> Because of this, it's best to use the version of the function below when reloading more than one shader.
 
----
-
+When you need to reload more than one shader program, pass an array of `bgfx.ShaderHandle` pointers into `reload`:
 ```d
-void reload(R)(R programs)
-if(hasAssignableElements!R && is(ElementType!R == bgfx.ProgramHandle*));
+auto myShaderProg1 = myShaders.load("myShaderA", "myShaderA");
+auto myShaderProg2 = myShaders.load("myShaderA", "myShaderB");
+auto myShaderProg3 = myShaders.load("myShaderC", "myShaderB");
+auto myShaderProg4 = myShaders.load("myShaderC", "myShaderA");
+
+myShaders.reload([&myShaderProg1, &myShaderProg2, &myShaderProg3, &myShaderProg4]);
 ```
-Reloads `programs`' shader files afresh from the disk and sets the handles in `programs` to newly created `bgfx.ProgramHandle`s.
-Example:
+It reloads the shaders of each passed shader program afresh from the disk, and sets each handle to a newly-created program handle.
+
+## Un-Loading
+When you're done with a certain shader program, you can use `unload` on it:
 ```d
-auto myShader1 = shelper.load("myShaderA", "myShaderA");
-auto myShader2 = shelper.load("myShaderA", "myShaderB");
-auto myShader3 = shelper.load("myShaderC", "myShaderB");
-auto myShader4 = shelper.load("myShaderC", "myShaderA");
-
-//sometime later:
-shelper.reload([
-	&myShader1,
-	&myShader2,
-	&myShader3,
-	&myShader4,
-]);
+myShaders.unload(myShaderProgram);
 ```
+This destroys `myShaderProgram`, but only cleans up its underlying `bgfx.ShaderHandle`s if there are no other `bgfx.ProgramHandle`s in the same Shelper instance using them.
 
----
-
+When you're about to shut down your program, run `unloadAll`:
 ```d
-bool unload(bgfx.ProgramHandle program) nothrow @nogc;
+myShaders.unloadAll();
 ```
-Destroys `program`, and cleans up its underlying `bgfx.ShaderHandle`s if there are no other Shelper-managed `bgfx.ProgramHandle`s using them.
+This will destroy all `bgfx.ProgramHandle`s managed by that Shelper instance, and clean up all of their underlying `bgfx.ShaderHandle`s.
 
----
+## Documentation
 
-```d
-void unloadAllShaderPrograms() nothrow @nogc;
-```
-Destroys all `bgfx.ProgramHandle`s managed by Shelper, and cleans up all of their underlying `bgfx.ShaderHandle`s.
+Further documentation is available in the source code of this library.
--- source/shelper/package.d
+++ source/shelper/package.d
@@ -1,219 +1,343 @@
+/+
++               Copyright 2024 Aya Partridge
++ Distributed under the Boost Software License, Version 1.0.
++     (See accompanying file LICENSE_1_0.txt or copy at
++           http://www.boost.org/LICENSE_1_0.txt)
++/
 module shelper;
 
 import core.time;
-import std.exception, std.format, std.range.primitives, std.typecons;
+import std.format, std.path, std.range, std.traits, std.typecons;
 import bindbc.bgfx;
 
+///All Shelper exceptions extend this class.
 class ShelperException: Exception{
-	this(string msg, string file=__FILE__, size_t line=__LINE__){
+	this(string msg, string file=__FILE__, size_t line=__LINE__) nothrow pure @safe{
+		super(msg, file, line);
+	}
+}
+///Thrown when trying to load a shader file that is too large.
+class ShelperTooLongException: ShelperException{
+	this(string filePath, ulong fileSize, string file=__FILE__, size_t line=__LINE__) pure @safe{
+		super(format!"Shader file '%s' is too long (%s bytes), must be at most %s bytes."(filePath, fileSize, uint.max), file, line);
+	}
+}
+///Thrown when bgfx can't create a shader/program with the relevant input.
+class ShelperCreationException: ShelperException{
+	this(string msg, string file=__FILE__, size_t line=__LINE__) nothrow pure @safe{
 		super(msg, file, line);
 	}
 }
 
-string shaderPath = "shaders/";
+/**
+Loads a non-managed shader.
 
+Should only be used when you don't want Shelper's convenient unloading/reloading functionality.
+
+Throws:
+	- `ShelperTooLongException` if the file size won't fit in a `uint`.
+	- `ShelperCreationException` if bgfx fails to create the shader.
+*/
+bgfx.ShaderHandle loadShader(string filePath){
+	import std.file: getSize;
+	const fileSize = filePath.getSize();
+	if(fileSize >= uint.max) throw new ShelperTooLongException(filePath, fileSize);
+	
+	auto memory = cast(bgfx.Memory*)bgfx.alloc(cast(uint)fileSize);
+	{
+		import std.stdio;
+		auto file = File(filePath.array, "rb");
+		const readResult = file.rawRead(memory.data[0..fileSize]);
+		assert(readResult.length == memory.size);
+	}
+	
+	auto shader = bgfx.createShader(memory);
+	if(shader == bgfx.invalidHandle!ShaderHandle) throw new ShelperCreationException(format!"bgfx failed to create a shader from file '%s'."(filePath));
+	return shader;
+}
+
+///Internal storage used to keep track of a shader handle.
 struct ShaderData{
-	const bgfx.ShaderHandle handle = bgfx.invalidHandle!ShaderHandle;
-	const ShaderKey key;
+	bgfx.ShaderHandle handle = bgfx.invalidHandle!ShaderHandle;
+	ShaderKey key;
 	size_t refCount = 1;
 }
 
+///A key used to look up a shader handle.
 struct ShaderKey{
 	string name;
 	MonoTime timestamp;
 }
 
-private{
-	ShaderData*[ShaderKey] allLoadedShaders;
-	ShaderData*[string] recentLoadedShaders;
+///Internal storage used to keep track of a shader program's underlying shaders.
+struct ProgramData{
+	bool isCompute = false;
+	union{
+		struct{
+			ShaderKey vert, frag;
+		}
+		struct{
+			ShaderKey comp;
+		}
+	}
+}
+
+struct Shelper(string vertFmt="%s.vert", string fragFmt="%s.frag", string compFmt="%s.comp"){
+	static assert(vertFmt != fragFmt && fragFmt != compFmt && vertFmt != compFmt, "The same format must not be used for two different types of shader");
 	
-	bgfx.ShaderHandle loadShader(string filePath){
-		import std.file;
-		enforce(filePath.exists, new ShelperException(format("Shader file '%s' not found", filePath)));
-		enforce(filePath.isFile, new ShelperException(format("Attempted to load shader from '%s', but it's not a file", filePath)));
+	private{
+		ShaderData[ShaderKey] allLoadedShaders;
+		ShaderData*[string] recentLoadedShaders;
 		
-		const memory = bgfx.copy(cast(void*)filePath.read(), cast(uint)filePath.getSize());
+		ProgramData[bgfx.ProgramHandle] shaderPrograms;
 		
-		auto shader = bgfx.createShader(memory);
-		enforce(shader != bgfx.invalidHandle!ShaderHandle, new ShelperException(format("Shader '%s' did not load properly.", filePath)));
-		return shader;
+		string _path = "shaders/";
+		string _pathAbs = null;
 	}
 	
-	bgfx.ShaderHandle getNewShader(string name, out ShaderKey key, ShaderData*[string] recent){
-		const handle = loadShader(shaderPath ~ name ~ ".bin");
-		key = ShaderKey(name, MonoTime.currTime);
-		auto data = new ShaderData(handle, key);
-		allLoadedShaders[key] = data;
-		recent[name] = data;
-		return handle;
+	///A relative or absolute file path to your shader binaries.
+	@property path() nothrow @nogc pure @safe => _path;
+	///ditto
+	@property path(string val) nothrow @nogc pure @safe{
+		_pathAbs = null;
+		_path = val;
 	}
 	
-	bgfx.ShaderHandle getShader(string name, out ShaderKey key, ShaderData*[string] recent){
-		if(auto oldShader = name in recent){
-			(*oldShader).refCount++;
-			key = (*oldShader).key;
-			return (*oldShader).handle;
+	///An absolute file path to your shader binaries.
+	@property pathAbs() @safe{
+		if(_pathAbs) return _pathAbs;
+		
+		_pathAbs = asAbsolutePath(_path).array;
+		return _pathAbs;
+	}
+	
+	///How many shaders this instance is using.
+	@property length() nothrow @nogc pure @safe => allLoadedShaders.length;
+	
+	private{
+		///Unconditionally load a shader from the disk
+		bgfx.ShaderHandle getNewShader(string name, out ShaderKey key, ShaderData*[string] recent){
+			const handle = loadShader(buildPath(pathAbs, name~".bin"));
+			key = ShaderKey(name, MonoTime.currTime);
+			allLoadedShaders[key] = ShaderData(handle, key);
+			recent[name] = key in allLoadedShaders;
+			return handle;
 		}
 		
-		return getNewShader(name, key, recent);
-	}
-	
-	void unloadShader(ShaderKey key) nothrow @nogc{
-		if(auto data = key in allLoadedShaders){
-			if(--(*data).refCount <= 0){
-				bgfx.destroy((*data).handle);
-				allLoadedShaders.remove(key);
-				
-				if(auto recentData = key.name in recentLoadedShaders){
-					if(*recentData is *data){
-						recentLoadedShaders.remove(key.name);
+		///Load a shader from the disk, unless it has already been loaded
+		bgfx.ShaderHandle getShader(string name, out ShaderKey key, ShaderData*[string] recent){
+			if(auto oldShader = name in recent){
+				(*oldShader).refCount++;
+				key = (*oldShader).key;
+				return (*oldShader).handle;
+			}
+			return getNewShader(name, key, recent);
+		}
+		
+		void unloadShader(ShaderKey key) nothrow @nogc{
+			if(auto data = key in allLoadedShaders){
+				if(--data.refCount <= 0){
+					bgfx.destroy(data.handle);
+					allLoadedShaders.remove(key);
+					
+					if(auto recentData = key.name in recentLoadedShaders){
+						if(*recentData is data){
+							recentLoadedShaders.remove(key.name);
+						}
 					}
 				}
 			}
 		}
-	}
-	
-	bgfx.ShaderHandle reloadShader(ref ShaderKey key){
-		unloadShader(key);
-		return getNewShader(key.name, key, recentLoadedShaders);
-	}
-	
-	bgfx.ShaderHandle[] reloadShaders(R)(ref R[] keys)
-	if(hasAssignableElements!R && is(ElementType!R == ShaderKey)){
-		foreach(key; keys){
+		
+		bgfx.ShaderHandle reloadShader(ref ShaderKey key){
 			unloadShader(key);
-		}
-		auto ret = new bgfx.ShaderHandle[](keys.length);
-		foreach(i, ref key; keys){
-			ret[i] = getNewShader(key.name, key, recentLoadedShaders);
-		}
-		return ret;
-	}
-}
-
-struct ProgramData{
-	ShaderKey vert, frag, comp;
-}
-
-ProgramData[bgfx.ProgramHandle] shaderPrograms;
-
-bgfx.ProgramHandle load(string vert, string frag){
-	ProgramData data;
-	auto handle = bgfx.createProgram(
-		getShader(vert~".vert", data.vert, recentLoadedShaders),
-		getShader(frag~".frag", data.frag, recentLoadedShaders),
-		false,
-	);
-	enforce(handle != invalidHandle!ProgramHandle, new ShelperException(format("Failed to create program with vert shader '%s', and frag shader '%s'.", data.vert, data.frag)));
-	shaderPrograms[handle] = data;
-	return handle;
-}
-
-bgfx.ProgramHandle load(string comp){
-	ProgramData data;
-	auto handle = bgfx.createProgram(
-		getShader(comp~".comp", data.comp, recentLoadedShaders),
-		false,
-	);
-	enforce(handle != invalidHandle!ProgramHandle, new ShelperException(format("Failed to create program with shader '%s'.", data.comp)));
-	shaderPrograms[handle] = data;
-	return handle;
-}
-
-void reload(ref bgfx.ProgramHandle program){
-	if(auto data = program in shaderPrograms){
-		bgfx.destroy(program);
-		
-		bgfx.ProgramHandle newHandle;
-		
-		if(data.comp.name is null){
-			newHandle = bgfx.createProgram(
-				reloadShader(data.vert),
-				reloadShader(data.frag),
-				false,
-			);
-			enforce(newHandle != invalidHandle!ProgramHandle, new ShelperException(format("Failed to re-create program with vert shader '%s', and frag shader '%s'.", data.vert, data.frag)));
-		}else{
-			newHandle = bgfx.createProgram(
-				reloadShader(data.comp),
-				false,
-			);
-			enforce(newHandle != invalidHandle!ProgramHandle, new ShelperException(format("Failed to re-create program with shader '%s'.", data.comp)));
+			return getNewShader(key.name, key, recentLoadedShaders);
 		}
 		
-		shaderPrograms.remove(program);
-		program = newHandle;
-		shaderPrograms[program] = *data;
-	}else{
-		program = bgfx.invalidHandle!ProgramHandle;
-	}
-}
-
-void reload(R)(R programs)
-if(hasAssignableElements!R && is(ElementType!R == bgfx.ProgramHandle*)){
-	auto oldData = new Nullable!(ProgramData)[](programs.length);
-	
-	//unload all old shaders at once, so that we'll only create one set of new ones
-	foreach(i, ref program; programs){
-		if(auto data = *program in shaderPrograms){
-			oldData[i] = *data;
-			bgfx.destroy(*program);
-			if(data.comp.name is null){
-				unloadShader(data.vert);
-				unloadShader(data.frag);
-			}else{
-				unloadShader(data.comp);
+		void reloadShaders(R)(ref R[] keys)
+		if(hasAssignableElements!R && is(ElementType!R == ShaderKey)){
+			foreach(key; keys){
+				unloadShader(key);
 			}
-			shaderPrograms.remove(*program);
+			foreach(i, ref key; keys){
+				getNewShader(key.name, key, recentLoadedShaders);
+			}
+			return ret;
 		}
 	}
 	
-	//load the new shaders & programs
-	ShaderData*[string] mostRecentShaders;
-	foreach(i, nData; oldData){
-		if(!nData.isNull){
-			auto data = nData.get();
+	/**
+	Create a shader program with vertex & fragment shader.
+	
+	If either of the underlying shaders are already loaded, then they will be
+	re-used rather than being freshly loaded from the disk.
+	
+	Params:
+		vertName = The filename of the vertex shader file, without its file extension.
+		fragName = The filename of the fragment shader file, without its file extension.
+	Returns: A newly created shader program handle.
+	
+	Throws: `ShelperCreationException` if bgfx fails to create the new shader(s) or shader program.
+	*/
+	bgfx.ProgramHandle load(string vertName, string fragName){
+		auto data = ProgramData(isCompute: false);
+		auto handle = bgfx.createProgram(
+			getShader(format!vertFmt(vertName), data.vert, recentLoadedShaders),
+			getShader(format!fragFmt(fragName), data.frag, recentLoadedShaders),
+			false,
+		);
+		if(handle == invalidHandle!ProgramHandle) throw new ShelperCreationException(format!"bgfx failed to create a program with vertex shader '%s' and fragment shader '%s'."(data.vert, data.frag));
+		shaderPrograms[handle] = data;
+		return handle;
+	}
+	
+	/**
+	Create a shader program with vertex & fragment shader.
+	
+	If the underlying shader is already loaded, then it will be
+	re-used rather than being freshly loaded from the disk.
+	
+	Params:
+		compName = The filename of the compute shader file, without its file extension.
+	Returns: A newly created shader program handle.
+	
+	Throws: `ShelperCreationException` if bgfx fails to create a new shader or the shader program.
+	*/
+	bgfx.ProgramHandle load(string compName){
+		auto data = ProgramData(isCompute: true);
+		auto handle = bgfx.createProgram(
+			getShader(format!compFmt(compName), data.comp, recentLoadedShaders),
+			false,
+		);
+		if(handle == invalidHandle!ProgramHandle) throw new ShelperCreationException(format!"bgfx failed to create a program with compute shader '%s'."(data.comp));
+		shaderPrograms[handle] = data;
+		return handle;
+	}
+	
+	/**
+	Reloads `program`'s shaders from the disk, and sets `program` to a newly-created program handle.
+	
+	If `program` was not originally loaded with this `Shelper` instance, it is set to `bgfx.invalidHandle!ProgramHandle`.
+	
+	Note:
+		This function always creates new underlying `bgfx.ShaderHandle`(s), but the old one(s) will only be destroyed if
+		no other `bgfx.ProgramHandle` in this `Shelper` instance is using them.
+		Because of this, it's best to use the the multi-reload overload when reloading more than one shader program.
+	
+	Throws: `ShelperCreationException` if bgfx fails to create the new shader(s) or shader program.
+	*/
+	void reload(ref bgfx.ProgramHandle program){
+		if(auto data = program in shaderPrograms){
+			bgfx.destroy(program);
+			
 			bgfx.ProgramHandle newHandle;
-			if(data.comp.name is null){
+			
+			if(!data.isCompute){
 				newHandle = bgfx.createProgram(
-					getShader(data.vert.name, data.vert, mostRecentShaders),
-					getShader(data.frag.name, data.frag, mostRecentShaders),
+					reloadShader(data.vert),
+					reloadShader(data.frag),
 					false,
 				);
+				if(newHandle == invalidHandle!ProgramHandle) throw new ShelperCreationException(format!"bgfx failed to re-create a program with vertex shader '%s' and fragment shader '%s'."(data.vert, data.frag));
 			}else{
 				newHandle = bgfx.createProgram(
-					getShader(data.comp.name, data.comp, mostRecentShaders),
+					reloadShader(data.comp),
 					false,
 				);
+				if(newHandle == invalidHandle!ProgramHandle) throw new ShelperCreationException(format!"bgfx failed to re-create program with compute shader '%s'."(data.comp));
 			}
-			shaderPrograms[newHandle] = data;
-			*programs[i] = newHandle;
+			
+			shaderPrograms.remove(program);
+			program = newHandle;
+			shaderPrograms[program] = *data;
 		}else{
-			*programs[i] = bgfx.invalidHandle!ProgramHandle;
+			program = bgfx.invalidHandle!ProgramHandle;
 		}
 	}
 	
-	//merge our new shaders into `recentLoadedShaders`
-	foreach(key, val; mostRecentShaders){
-		recentLoadedShaders[key] = val;
+	/**
+	Reloads each shader program pointed to in `programs` from the disk.
+	
+	If a program was not originally loaded with this `Shelper` instance, it is set to `bgfx.invalidHandle!ProgramHandle`.
+	
+	Throws: `ShelperCreationException` if bgfx fails to create the new shaders or shader programs.
+	*/
+	void reload(R)(R programs)
+	if(hasAssignableElements!R && is(ElementType!R == bgfx.ProgramHandle*)){
+		auto oldData = new Nullable!(ProgramData)[](programs.length);
+		
+		//unload all old shaders at once, so that we'll only create one set of new ones
+		foreach(i, ref program; programs){
+			if(auto data = *program in shaderPrograms){
+				oldData[i] = *data;
+				bgfx.destroy(*program);
+				if(!data.isCompute){
+					unloadShader(data.vert);
+					unloadShader(data.frag);
+				}else{
+					unloadShader(data.comp);
+				}
+				shaderPrograms.remove(*program);
+			}
+		}
+		
+		//load the new shaders & programs
+		ShaderData*[string] mostRecentShaders;
+		foreach(i, nData; oldData){
+			if(!nData.isNull){
+				auto data = nData.get();
+				bgfx.ProgramHandle newHandle;
+				if(!data.isCompute){
+					newHandle = bgfx.createProgram(
+						getShader(data.vert.name, data.vert, mostRecentShaders),
+						getShader(data.frag.name, data.frag, mostRecentShaders),
+						false,
+					);
+				}else{
+					newHandle = bgfx.createProgram(
+						getShader(data.comp.name, data.comp, mostRecentShaders),
+						false,
+					);
+				}
+				shaderPrograms[newHandle] = data;
+				*programs[i] = newHandle;
+			}else{
+				*programs[i] = bgfx.invalidHandle!ProgramHandle;
+			}
+		}
+		
+		//merge our new shaders into `recentLoadedShaders`
+		foreach(key, val; mostRecentShaders){
+			recentLoadedShaders[key] = val;
+		}
 	}
-}
-
-bool unload(bgfx.ProgramHandle program) nothrow @nogc{
-	if(auto data = program in shaderPrograms){
-		bgfx.destroy(program);
-		unloadShader(data.vert);
-		unloadShader(data.frag);
-		return true;
+	
+	/**
+	Destroys `program`.
+	
+	Note:
+		Only cleans up the underlying `bgfx.ShaderHandle`(s) if there are no other
+		`bgfx.ProgramHandle`s in the same Shelper instance using them.
+	
+	Returns: `true` unless `program` doesn't exist or doesn't belong to this `Shelper` instance.
+	*/
+	bool unload(bgfx.ProgramHandle program) nothrow @nogc{
+		if(auto data = program in shaderPrograms){
+			bgfx.destroy(program);
+			unloadShader(data.vert);
+			unloadShader(data.frag);
+			return true;
+		}
+		return false;
 	}
-	return false;
-}
-
-void unloadAllShaderPrograms() nothrow @nogc{
-	foreach(program; shaderPrograms.byKey()){
-		unload(program);
+	
+	///Destroys all shader programs from this `Shelper` instance, and cleans up all of its underlying `bgfx.ShaderHandle`s.
+	void unloadAll() nothrow @nogc
+	out(; allLoadedShaders.length == 0)
+	out(; recentLoadedShaders.length == 0){
+		foreach(program; shaderPrograms.byKey()){
+			unload(program);
+		}
+		shaderPrograms = null;
 	}
-	shaderPrograms = null;
-	assert(allLoadedShaders.length == 0);
-	assert(recentLoadedShaders.length == 0);
 }
